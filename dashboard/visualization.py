6c726118cdaa7d8af62798c8aeb5ca42a722876f
Amulah-98 <amalshariff98@gmail.com>
2022-08-22 16:51:53 +0300

    dashboard

---
+++ dashboard/visualization.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import sys
+import pickle
+
+
+st.title("Rosmann Pharmaceutical Sales Prediction")
+
+st.header("Distribution and Visualization of the pharmaceutical Data")
+
+st.subheader("Correlation Analysis")
+st.image('images/dashboard/corr.png')
+
+st.subheader("Analysis of storetypes on open days")
+st.image('images/dashboard/storetype.png')
+
+st.subheader("State Holiday customer Analysis")
+st.image('images/dashboard/stateholiday.png')
+
+st.subheader("Assortment Effect On Sales")
+st.image('images/dashboard/assortment.png')
+
+st.subheader("Mean monthly sales")
+st.image('images/dashboard/mean_monthly_sales.png')
+
+st.subheader("AutoCorrelation")
+st.image('images/dashboard/autocorr.png')
+
+st.subheader("Partial AutoCorrelation")
+st.image('images/dashboard/partial ac.png')
+
+st.header("Model Analysis")
+
+st.subheader("test and val loss plot")
+st.image('images/dashboard/xloss.png')
+
+st.subheader("Feature Importance on model")
+st.image('images/dashboard/features.png')
+
+st.subheader("Prediciton image - Time Series")
+st.image('images/dashboard/lstm.png')
